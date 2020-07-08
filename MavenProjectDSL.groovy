d64a07d6b7a75302253196eda1c0a51ed585fd1f
great <great@gmail.com>
2020-07-08 15:45:19 +0530

    commited groovy

--- MavenProjectDSL.groovy
+++ MavenProjectDSL.groovy
@@ -7,7 +7,7 @@ job('First-Maven-Project-Via-DSL') {
         scm('* * * * *')
     }
     steps {
-        maven('clean package', 'Maven-Project/single-module/pom.xml')
+        maven('clean package', 'single-module/pom.xml')
     }
     publishers {
         //archive the war file generated
